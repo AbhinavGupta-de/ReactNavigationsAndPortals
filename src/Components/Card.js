949a7e0af6021185d7382e39843e1e2a13e305e2
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-08-09 13:57:49 +0530

    Update the contact and added Card uit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,44 @@
-import React from 'react';
+// import React, { Component } from 'react';
+// import useParams from 'react-router-dom';
 
-const Card = () => {
+// export default class Card extends Component {
+// 	state = { user: '' };
+
+// 	componentDidMount() {
+// 		// let user = this.props.match.params.user;
+// 		// this.setState({ user: user });
+
+// 		const { user } = useParams();
+// 		console.log(this.props);
+// 	}
+// 	render() {
+// 		const { user } = this.state;
+// 		return (
+// 			<div
+// 				className="ui raised very padded text container segment"
+// 				style={{ marginTop: '80px' }}
+// 			>
+// 				<h1 className="ui header">Contact {user}</h1>
+// 				<p>
+// 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+// 					quibusdam.
+// 				</p>
+// 			</div>
+// 		);
+// 	}
+// }
+
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Card = (props) => {
+	const { user } = useParams();
 	return (
 		<div
 			className="ui raised very padded text container segment"
 			style={{ marginTop: '80px' }}
 		>
-			<h1 className="ui header">Card</h1>
+			<h1 className="ui header">Hello {user}</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
 				quibusdam.
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Contact = (props) => {
-	console.log(props);
 	return (
-		<div
-			className="ui raised very padded text container segment"
-			style={{ marginTop: '80px' }}
-		>
-			<h1 className="ui header">Contact</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-				quibusdam.
-			</p>
+		<div>
+			<div
+				className="ui raised very padded text container segment"
+				style={{ marginTop: '80px' }}
+			>
+				<Link to={'/card/Abhinav'} className="ui header">
+					Abhinav
+				</Link>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+					quibusdam.
+				</p>
+			</div>
+			<div
+				className="ui raised very padded text container segment"
+				style={{ marginTop: '80px' }}
+			>
+				<Link to={'/card/Bot'} className="ui header">
+					Bot
+				</Link>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+					quibusdam.
+				</p>
+			</div>
 		</div>
 	);
 };
