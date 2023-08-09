5a7a010b8deccc599a43ea10777842c4602d34e5
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-08-09 15:22:48 +0530

    created a Modal js

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const About = () => {
 	return (
-		<div
-			className="ui raised very padded text container segment"
-			style={{ marginTop: '80px' }}
-		>
-			<h1 className="ui header">About us</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-				quibusdam.
-			</p>
+		<div>
+			{/* <div className="modal">
+				<div className="modal-content">
+					<h3>I am a modal</h3>
+				</div>
+			</div> */}
+			<div
+				className="ui raised very padded text container segment"
+				style={{ marginTop: '80px' }}
+			>
+				<h1 className="ui header">About us</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+					quibusdam.
+				</p>
+			</div>
 		</div>
 	);
 };
